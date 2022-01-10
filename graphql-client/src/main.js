fee5a6a44486b9782dfe33d8b286448b195b6ebb
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-09 22:25:37 -0300

    create client

---
+++ graphql-client/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+import ApolloClient from 'apollo-boost'
+import VueApollo from "vue-apollo";
+
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: 'http://localhost:4001'
+  })
+});
+
+Vue.use(VueApollo); 
+
+new Vue({
+  el: '#app',
+  apolloProvider,
+  render: h => h(App)
+})
